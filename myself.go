62bb9892c7888160723e91877dafc6ee1b3347d9
unknown <shilpaajeeshm>
2020-12-18 11:53:57 +0530

    Day1

---
+++ myself.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Shilpa Ajeesh M "
+	var id int = 1
+	var mark float32 = 20.5
+	fmt.Printf("My name is %s my ID is %d also my mark is %f", name, id, mark)
+}
